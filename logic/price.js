f39aab1e09f89b0043c18a220c12574cfc7e9bc7
Linnea Kols <linnea.kols@knowit.se>
2023-02-10 14:15:55 +0100

    Module 3 completed

--- logic/price.js
+++ logic/price.js
@@ -1,4 +1,37 @@
+"use strict"
 function Prices() {
 
+    function calculateFinalPrice(basePrice, variationPassengerType, variationFlightType) {
+        const finalPrice = (basePrice + variationPassengerType) * (basePrice + variationFlightType) / 100
+        return finalPrice
+    }
+
+    function calculateDefaultFinalPrice(basePrice, passengerType, flightType) {
+        let variationPassenger = 0
+        let variationFlight = 0
+        if (passengerType.toUpperCase() == "REGULAR") {
+            variationPassenger = -5
+        } else if (passengerType.toUpperCase() == "VIP") {
+            variationPassenger = 5
+        }
+
+        if (flightType.toUpperCase() == "ECONOMY") {
+            variationFlight = -3
+        } else if (flightType.toUpperCase() == "BUSINESS") {
+            variationFlight = 10
+        }
+
+        const finalPrice = (basePrice + variationPassenger) * (basePrice + variationFlight) / 100
+        return finalPrice
+    }
+
+    function calculateTotalFinalPrice(numberOfSeats, passengerType, flightType, basePrice) {
+        const finalPrice = numberOfSeats * calculateDefaultFinalPrice(basePrice, passengerType, flightType)
+        return finalPrice
+    }
+
+    return {calculateFinalPrice, calculateDefaultFinalPrice, calculateTotalFinalPrice}
 }
+
+module.exports = Prices()
 
--- logic/util.js
+++ logic/util.js
@@ -1,3 +1,4 @@
+"use strict"
 function Util() {
 
     function calculateTotalDistributedPassengers(passengerDistribution) {
@@ -11,7 +12,32 @@ function Util() {
         return amountOfPassengers.reduce((accumulator, currentValue) => accumulator + currentValue, 0)
     }
 
-    return {calculateTotalDistributedPassengers, calculateTotalNumberOfPassengers}
+    function checkInput(input) {
+        if (!Number.isInteger(input) || input == null) {
+            throw new Error("Input is null or not a number")
+        }
+    }
+
+    function calculateTotalDistance(distances) {
+        let sum = 0
+        for (let num = 0; num < distances.length; num++) {
+            if(distances[num] < 0) {
+                continue
+            }
+            sum += distances[num]
+        }
+        return sum
+    }
+
+    function calculateBonusPoints(businessDistances, economyDistances, businessBonus, economyBonus) {
+        const businessPoints = calculateTotalDistance(businessDistances) * businessBonus
+        const economyPoints = calculateTotalDistance(economyDistances) * economyBonus
+        return (businessPoints + economyPoints) / 100
+    }
+
+
+    return {calculateTotalDistributedPassengers, calculateTotalNumberOfPassengers,
+        checkInput, calculateTotalDistance, calculateBonusPoints}
 }
 
 module.exports = Util()
